d85646f30169ec138a7d53c0accac01712001465
Ben Abbott <babbott85@gmail.com>
2021-06-26 11:57:04 -0600

    added navbar and footer component

---
+++ src/components/footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Footer (){
+    return(
+        <footer>
+            Memory Notes 2021
+            All Rights Reserved 
+
+        </footer>
+    )
+}
+
+export default Footer
