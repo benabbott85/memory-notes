7415d5f3e5b05717edcdcd13e0ecd461074d9fe7
Ben Abbott <babbott85@gmail.com>
2021-06-30 22:08:19 -0600

    basic contact form on page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -3,7 +3,17 @@ import React from "react"
 
 function Contact(){
     return(
+        <>
         <h1>Contact Us</h1>
+        <div>
+            <form>
+            <p>Name: <input type="text" placeholder="Your Name Here"/></p>
+            <p>Email: <input type="text" placeholder="Your Email Here"/></p>
+            <button type="submit">Send</button>
+
+            </form>
+        </div>
+        </>
     )
 
 
